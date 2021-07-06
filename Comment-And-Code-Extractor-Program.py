469b20162a78c7b8005087140b721c200da7d1c2
orbitze <mverma1427@gmail.com>
2021-07-06 23:10:24 +0530

    Add comments to the code

--- Comment-And-Code-Extractor-Program.py
+++ Comment-And-Code-Extractor-Program.py
@@ -1,95 +1,108 @@
+# -----------------------------------------------------------------
+# |                    SYSTEMS-2 ASSIGNMENT-2                     |
+# |---------------------------------------------------------------|
+# | Date: July 5, 2021                                            |  
+# | Submitted by: MEGHA VERMA                                     |                                   |                                            
+# |_______________________________________________________________|
+
+
 import sys
 
-# File input from the user
-# Path of the file is also accepted
+
+# Function to write a given line into the 'codefile'
+def writeToCodeFile(line):
+    code_txt = " " + line + "\n" 
+    codefile.write(code_txt)
+
+
+# Function to write a given line into the 'commentfile'
+# Case parameter dictates in what format the comment will be stored
+def writeToCommentFile(i, line, case):
+    if case == 0:
+        comment_txt = str(i) + "  " + line.strip() + "\n"  
+    # if continued multi-line comment line
+    elif case == 1:
+        comment_txt = str(i) + "     " + line.strip() + "\n" 
+    # if line contains code, store a single dot in its place
+    elif case == 2:
+        comment_txt = str(i) + "  .\n"
+    
+    commentfile.write(comment_txt)
+
+
+
+# File input from the user (path of the file is also accepted)
 input = sys.argv[1]
-file = open(input, "r")
 
 # Opening 2 text files to store the code and the comments separately
-codefile = open("Original.txt","w")
+codefile    = open("Original.txt","w")
 commentfile = open("Comments.txt","w")
 
-# Flag variable to keep track of the multi-line comments
-commentInContinuation = False
 
-# Variable to keep track of line numbers in the C source file
-i = 1
+sourcefile = open(input, "r")   # Creating the file object named 'file'
+commentInContinuation = False   # Flag variable to keep track of the multi-line comments
+i = 1                           # Variable to keep track of line numbers in the C source file
+
 
 # Traversing through the C source file 'line by line'
-for line in file:
+for line in sourcefile:
 
     # Storing the first 2 chars (ignoring spaces) in the line
-    # to see whether it is a comment or a line of code
+    # to see if it's a comment or a line of code
     lineBeginsWith = line.strip()[:2]
 
-
-    # if the line starts with a 'single line' comment symbol
-    if (lineBeginsWith == '//'):
-        # Storing this comment to write into the 'commentfile'
-        comment_txt = str(i) + "  " + line.strip() + "\n"         
+    if (lineBeginsWith == '//'):   
+        writeToCommentFile(i,line,0)
+        writeToCodeFile("")      
 
 
-    # if Case(1) the line starts with a 'multi-line' comment start symbol
-    # Or Case(2) the line is a part of a previously encountered multi-line comment
+    # if Case(1) the line starts with a 'multi-line' comment start symbol, OR
+    # if Case(2) the line is a part of a previously encountered multi-line comment
     elif (lineBeginsWith == '/*' or commentInContinuation == True):
-        # if Case(1)
+
         if (lineBeginsWith == '/*'):
-            comment_txt = str(i) + "  " + line.strip() + "\n"
-            # Updating the flag to indicate that the next line contains
-            # part of the multi-line comment
+            # Updating the flag to indicate that multi-line comment is continued on next line
             commentInContinuation = True
-        
-        # if Case(2)
+            writeToCommentFile(i,line,0)
+
         else:
             # if multi-line comment end symbol '*/' is encountered at the beginning
             if (line.strip() == '*/'):
-                comment_txt = str(i) + "  " + line.strip() + "\n" 
+                writeToCommentFile(i,line,0)
             else:
-                comment_txt = str(i) + "     " + line.strip() + "\n" 
+                writeToCommentFile(i,line,1)
+
         
-        # if multi-line comment end symbol '*/' is encountered at the end of a line
+        # Updating the flag to indicate that multi-line comment has ended
+        # if multi-line comment end symbol is encountered at the end of the line
         if (line.strip()[-2:] == '*/'):
-            # Updating the flag to indicate that multi-line comment has ended
             commentInContinuation = False
 
-
-    # if the comment symbol is present within the same line as the code line 
+        writeToCodeFile("") 
+ 
     elif ('//' in line or '/*' in line):
-        # if it's a single line comment symbol
+        # Variable partitionPt stores the index that separates the 
+        # line of code from the comment that is followed after in the same line
         if ('//' in line):
-            start = line.index('//') 
-
-        # if it's a multi-line comment symbol
+            partitionPt = line.index('//')
         elif ('/*' in line):
-            start = line.index('/*') 
-    
-        # Storing the comment part of the line to write into the 'commentfile'
-        comment_txt = str(i) + "  " + line[start:].strip() + "\n"
-        # Storing the code part of the line to write into the 'codefile'
-        code_txt = " " + line[:start] + "\n"
-         # Writing into the 'codefile'
-        codefile.write(code_txt)
-    
+            partitionPt = line.index('/*') 
 
-    # if none of the above cases are true then
-    # it must be the case that it is a code line 
+        writeToCommentFile(i,line[partitionPt:],0)  
+        writeToCodeFile(line[:partitionPt]) 
+
+
+    # if none of the above cases are true then the line must contain only the code part 
     else:
-        # if instead of a code line it's an empty line
-        code_txt = " " + line
-        # if it is a code line indeed
+        # if it is a line of code
         if (len(line.strip()) != 0):
-            code_txt += "\n"
-        # Storing the line of code to write into the 'codefile'
-        comment_txt = str(i) + "\n"
-        # Writing into the 'codefile'
-        codefile.write(code_txt)
+            writeToCodeFile(line)
 
-    # Writing the comment into the 'commentfile'
-    commentfile.write(comment_txt)
-
+        writeToCommentFile(i,line,2)
+       
     # Updating the line number
     i = i+1
 
 
 # Closing the C source file
-file.close()
+sourcefile.close()
